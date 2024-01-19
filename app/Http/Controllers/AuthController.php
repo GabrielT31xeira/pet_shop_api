4922ee5f4a2a8013e26f7e9cd9320c476e16553d
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2024-01-19 17:36:45 -0300

    Finishi the auth and add roles to user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -87,24 +87,31 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-            'role_id' => 'required'
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+                'role_id' => 'required'
+            ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'role_id' => $request->role_id
-        ]);
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'role_id' => $request->role_id
+            ]);
 
-        return response()->json([
-            'message' => 'User created successfully',
-            'user' => $user
-        ]);
+            return response()->json([
+                'message' => 'User created successfully',
+                'user' => $user
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error has occurred',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
     /**
      * @OA\Post(
@@ -171,25 +178,32 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]);
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            return response()->json([
-                'user' => $user,
-                'authorization' => [
-                    'token' => $user->createToken('ApiToken')->plainTextToken,
-                    'type' => 'bearer',
-                ]
+        try {
+            $request->validate([
+                'email' => 'required|string|email',
+                'password' => 'required|string',
             ]);
-        }
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                return response()->json([
+                    'user' => $user,
+                    'authorization' => [
+                        'token' => $user->createToken('ApiToken')->plainTextToken,
+                        'type' => 'bearer',
+                    ]
+                ]);
+            }
 
-        return response()->json([
-            'message' => 'Invalid credentials',
-        ], 401);
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error has occurred',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -210,10 +224,17 @@ class AuthController extends Controller
      */
     public function logout()
     {
-        Auth::user()->tokens()->delete();
-        return response()->json([
-            'message' => 'Successfully logged out',
-        ]);
+        try {
+            Auth::user()->tokens()->delete();
+            return response()->json([
+                'message' => 'Successfully logged out',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error has occurred',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function refresh()
